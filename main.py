bbc2d9a82a929590dd525cf06809c6cc223a0dd5
unknown <davidlfbvr19@gmail.com>
2019-12-11 13:02:20 +0100

    add GyroIn to main.py

--- main.py
+++ main.py
@@ -51,6 +51,14 @@ def encode(key , plain_text ): #Fonction chiffrant le message selon le chiffreme
         enc.append(enc_c)
     return ("".join(enc).encode()).decode()
 
+def decode(key, cipher_text): #Fonction dechiffrant le message selon le chiffrement vigenere
+    dec = []
+    for i, e in enumerate(cipher_text):
+        key_c = key[i % len(key)]
+        dec_c = chr((256 + ord(e) - ord(key_c)) % 256)
+        dec.append(dec_c)
+    return str("".join(dec))
+
 def hashing(list):
     string = ''.join(list)
     def to_32(value):
